4959cb87492de504614ef148b8e6f70db555a984
meer-1 <yours2312@gmail.com>
2024-09-02 08:31:55 +0530

    hyper paramter tunning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.logger import logging
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -20,30 +21,33 @@ def save_object(file_path,obj):
         logging.error(f"Failed to save object: {e}")
         raise CustomException(e, sys)
         
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-
-        report={}
-
+        report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train) # Train model
+            para=param[list(models.keys())[i]]
 
-            # Make predictions
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]]=test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
-        return report    
-        
-
+        return report
+    
     except Exception as e:
-        raise CustomException(e,sys)
-        
-        
+        raise CustomException(e, sys)
